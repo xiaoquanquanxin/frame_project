ebf7d8c56c5aa4135785582eab14ca431ec257fc
quanxin <3059210183@qq.com>
2019-03-14 16:28:39 +0800

    分次数提交

--- static/js/H-ui/H-ui.js
+++ static/js/H-ui/H-ui.js
@@ -4855,7 +4855,7 @@ jQuery(function($) {
 !function($) {
 	'use strict';
 	// MODAL CLASS DEFINITION
-	// ======================	
+	// ======================
 	var Modal = function(element, options) {
 		this.options = options;
 		this.$body = $(document.body);
@@ -5872,7 +5872,7 @@ $(function() {
 	$.fn.alert.Constructor = Alert;
 
 	// ALERT NO CONFLICT
-	// =================	
+	// =================
 	$.fn.alert.noConflict = function() {
 		$.fn.alert = old;
 		return this;
@@ -8992,6 +8992,34 @@ $(function() {
 		else $.error('Method ' + method + ' does not exist!');
 	};
 } (jQuery);
+
+/* =======================================================================
+ * jQuery.Huifocusblur.js v2.0 得到失去焦点
+ * http://www.h-ui.net/
+ * Created & Modified by guojunhui
+ * Date modified 2017.05.09
+ *
+ * Copyright 2017 北京颖杰联创科技有限公司 All rights reserved.
+ * Licensed under MIT license.
+ * http://opensource.org/licenses/MIT
+ * ========================================================================*/
+!function($) {
+	$.fn.Huifocusblur = function(options){
+		var defaults = {
+			className:"focus",
+		}
+		var options = $.extend(defaults, options);
+		this.each(function(){
+			var that = $(this);
+			that.focus(function() {
+				that.addClass(options.className).removeClass("inputError");
+			});
+			that.blur(function() {
+				that.removeClass(options.className);
+			});
+		});
+	}
+} (window.jQuery);
 
 $(function() {
 	$('.switch')['bootstrapSwitch']();
